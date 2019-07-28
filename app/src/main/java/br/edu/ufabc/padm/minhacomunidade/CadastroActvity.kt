b6a63594e2d510220efe870d910d7106d53070c5
Alexandre Magno <amagnomartins@icloud.com>
2019-07-28 20:16:22 -0300

    Cadastro

---
+++ app/src/main/java/br/edu/ufabc/padm/minhacomunidade/CadastroActvity.kt
@@ -0,0 +1,16 @@
+package br.edu.ufabc.padm.minhacomunidade
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+class CadastroActvity : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.cadastro_activity,)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
