f3c4135ea9f132c62ec81b64cc81cfb5486863d3
Vinicius <vsxavier@live.com>
2019-07-28 19:50:49 -0300

    Criando o layout do feed, problema com a action bar

--- app/src/main/java/br/edu/ufabc/padm/minhacomunidade/LoginActivity.kt
+++ app/src/main/java/br/edu/ufabc/padm/minhacomunidade/LoginActivity.kt
@@ -1,12 +1,31 @@
 package br.edu.ufabc.padm.minhacomunidade
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity:AppCompatActivity() {
 
+    private lateinit var loginBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+        init()
+    }
+
+    private fun init(){
+        loginBtn = findViewById(R.id.login_btn)
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        loginBtn.setOnClickListener {
+            startActivity(Intent(this,FeedActivity::class.java))
+        }
+
     }
 }
