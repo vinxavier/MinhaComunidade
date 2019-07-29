87b67c03ff997fbfc76bf706dd6d5db104f765be
Vinicius <vsxavier@live.com>
2019-07-29 16:50:36 -0300

    Adicionado tela para adicionar imagens ao projeto

---
+++ app/src/main/java/br/edu/ufabc/padm/minhacomunidade/NovoProjeto2Activity.kt
@@ -0,0 +1,14 @@
+package br.edu.ufabc.padm.minhacomunidade
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NovoProjeto2Activity: AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.novo_projeto_2)
+    }
+}
